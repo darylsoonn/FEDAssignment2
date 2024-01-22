9cc24e20c00485a5be762070636f8fc96e87ed74
daryl <s10258182@connect.np.edu.sg>
2024-01-23 02:16:00 +0800

    homepage update 1.3

--- java.js
+++ java.js
@@ -1,3 +1,6 @@
-$(document).ready(function () {
-    $('#imageCarousel').carousel();
+window.addEventListener('scroll', function () {
+    // Adjust the 0.5 multiplier based on how fast you want the blur effect to occur
+    var blurValue = Math.min(50, window.scrollY * 0.5);
+    document.querySelector('.image-background img').style.filter = 'blur(' + blurValue + 'px)';
 });
+
