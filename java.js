2a045c52988c6d0f806f56053ec3426f5920d245
daryl <s10258182@connect.np.edu.sg>
2024-01-26 03:10:43 +0800

    homepage update 1.4

--- java.js
+++ java.js
@@ -1,5 +1,4 @@
 window.addEventListener('scroll', function () {
-    // Adjust the 0.5 multiplier based on how fast you want the blur effect to occur
     var blurValue = Math.min(50, window.scrollY * 0.5);
     document.querySelector('.image-background img').style.filter = 'blur(' + blurValue + 'px)';
 });
