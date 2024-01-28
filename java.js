08cb69dbbcf225aa50e7f63969ab19c0dfe6d3f5
daryl <s10258182@connect.np.edu.sg>
2024-01-28 17:27:19 +0800

    login page and shop.html page plus api

--- java.js
+++ java.js
@@ -1,5 +1,36 @@
-window.addEventListener('scroll', function () {
-    var blurValue = Math.min(50, window.scrollY * 0.5);
-    document.querySelector('.image-background img').style.filter = 'blur(' + blurValue + 'px)';
-});
+function handleScroll() {
+    window.addEventListener('scroll', function () {
+        var blurValue = Math.min(50, window.scrollY * 0.5);
+        document.querySelector('.image-background img').style.filter = 'blur(' + blurValue + 'px)';
+    });
+}
 
+async function fetchData(productId) {  
+    try {  
+        const response = await fetch(`https://fakestoreapi.com/products/${productId}`);  
+        const data = await response.json();  
+        displayData(data); 
+    } catch (error) {  
+        console.error(error);  
+    }  
+  }  
+   
+  function displayData(data) {   
+    const dataContainer = document.getElementById('data-container');  
+    const newContainer = document.createElement('div'); 
+    newContainer.classList.add('data-container'); 
+   
+    newContainer.innerHTML = `  
+        <h2>${data.title}</h2>  
+        <p>${data.description}</p>  
+        <p>Price: $${data.price}</p>  
+        <img src="${data.image}" alt="${data.title}">  
+    `; 
+   
+    dataContainer.appendChild(newContainer); 
+  }  
+    
+  for (let productId = 1; productId <= 30; productId++) { 
+    fetchData(productId); 
+  }
+  
